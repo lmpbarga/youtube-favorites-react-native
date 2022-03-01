71dc87a6bfc51cb582ce4dd1383f3a4ecd363470
Lucas Barga <lucasmpbarga@gmail.com>
2022-03-01 11:59:42 -0300

    add yarn and remove package-lock

--- App.tsx
+++ App.tsx
@@ -16,6 +16,7 @@ import {
   Icon,
   Input,
   ScrollView,
+  IconButton,
 } from "native-base";
 import NativeBaseIcon from "./components/NativeBaseIcon";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
@@ -33,37 +34,53 @@ declare module "native-base" {
   interface ICustomTheme extends MyThemeType {}
 }
 
-
 function SearchBar() {
-  return <Input placeholder="Search People & Places" width="100%" borderRadius="4" py="3" px="1" fontSize="14" InputLeftElement={<Icon m="2" ml="3" size="6" color="gray.400" as={<MaterialIcons name="search" />} />} InputRightElement={<Icon m="2" mr="3" size="6" color="gray.400" as={<MaterialIcons name="mic" />} />} />
+  return (
+    <Input
+      placeholder="Search People & asdPlaces"
+      width="100%"
+      borderRadius="4"
+      py="3"
+      px="1"
+      fontSize="14"
+      InputLeftElement={
+        <IconButton colorScheme="red" size={20} variant="ghost" _icon={{
+          as: MaterialIcons,
+          name: "arrow-back"
+        }} />
+      }
+      InputRightElement={
+        <Icon
+          m="2"
+          mr="3"
+          size="6"
+          color="gray.400"
+          as={<MaterialIcons name="mic" />}
+        />
+      }
+    />
+  );
 }
-
-function Example() {
-  return <Center flex={1} px="2">
-      <SearchBar />
-    </Center>;
-}
-
 
 export default function App() {
   return (
     <NativeBaseProvider>
-
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Code>App.tsx</Code>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
+      <VStack space={5} alignItems="center" marginTop={100}>
+        <SearchBar />
+        <NativeBaseIcon />
+        <Heading size="lg">Welcome to NativeBase</Heading>
+        <HStack space={2} alignItems="center">
+          <Text>Edit</Text>
+          <Code>App.tsx</Code>
+          <Text>and save to reload.</Text>
+        </HStack>
+        <Link href="https://docs.nativebase.io" isExternal>
+          <Text color="primary.500" underline fontSize={"xl"}>
+            Learn NativeBase
+          </Text>
+        </Link>
+        <ToggleDarkMode />
+      </VStack>
     </NativeBaseProvider>
   );
 }
